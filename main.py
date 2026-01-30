9ac29e418b15087dbe1243aac5844f0bdabdda35
best-ofme <kitiponglianghiranthawon@gmail.com>
2026-01-30 15:38:02 +0700

    Initial commit for OOP682 project

---
+++ main.py
@@ -0,0 +1,15 @@
+from models.student import Student
+from models.staff import Staff
+from models.person import Person
+
+def main():
+    person01 = Person("P005", "Asa", 20)
+    student1 = Student("P001", "Somchai", 20, "S12345")
+    staff1 = Staff("P002", "Suda", 35, "ST6789")
+
+    print(student1)
+    print(staff1)
+    print(person01)
+
+if __name__ == "__main__":
+    main()
